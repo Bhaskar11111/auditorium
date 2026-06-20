633a188f3876e30df3cb14aa9370b7081c2d5865
Bhaskar11111 <bhaskarmishra911@gmail.com>
2026-06-21 03:19:14 +0530

    deployment 1.1

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 const cors=require('cors')
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://auditorium-air.onrender.com',
     credentials:true
 }))
 app.use(express.json())
